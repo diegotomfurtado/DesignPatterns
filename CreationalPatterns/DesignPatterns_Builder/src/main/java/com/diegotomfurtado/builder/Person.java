9067356b427938664e3a7780f3b153fa15a28115
“diego.furtado” <diegotomfurtado@gmail.com>
2019-06-21 14:20:59 -0300

    Creating a design patterns repository (already inclued Builder)

---
+++ CreationalPatterns/DesignPatterns_Builder/src/main/java/com/diegotomfurtado/builder/Person.java
@@ -0,0 +1,46 @@
+package com.diegotomfurtado.builder;
+
+public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String middleName, String lastName, int age) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public Person(String firstName, String lastName, int age) {
+        this(firstName, null, lastName, age);
+    }
+    public Person(String firstName, int age) {
+        this(firstName, null, age);
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
