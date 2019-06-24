28e622ef2ef30805c4dd24b8bdc78d7471140dc9
diegotomfurtado <diegotomfurtado@gmail.com>
2019-06-23 23:46:46 -0300

    Fixing the path and packages in Gradle project

---
+++ CreationalPatterns/BuilderPatterns/src/main/java/builderpatterns/builder/PersonWithBuilder.java
@@ -0,0 +1,79 @@
+package builderpatterns.builder;
+
+
+public class PersonWithBuilder {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private int age;
+	
+	
+	public PersonWithBuilder(
+			String firstName, 
+			String middleName, 
+			String lastName, 
+			int age
+			) 
+	{
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+	public String getLastName() {
+		
+		return lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+
+	public static class Builder {
+        private String firstName;
+        private String middleName;
+        private String lastName;
+        private int age;
+
+        public Builder(){}
+        
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonWithBuilder build() {
+            return new PersonWithBuilder(
+            		firstName, 
+            		middleName, 
+            		lastName, 
+            		age
+            );
+        }
+    }
+}
